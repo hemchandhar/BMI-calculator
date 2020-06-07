3405c10b3d1a3d3b62d85b6997295e4336423a1f
hemchandhar <hemchandhar@gmail.comm>
2020-06-07 07:01:55 +0530

    app-finished

---
+++ lib/components/icon_content.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({this.mainIcon, this.mainText});
+  final IconData mainIcon;
+  final String mainText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          mainIcon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          mainText,
+          style: kLabelTextStyle,
+        ),
+      ],
+    );
+  }
+}
